664baf3268b65d78abd2a11d464b60e0b359cf69
Liam <liamearley7@protonmail.com>
2026-02-10 15:23:43 -0500

    Exclude fields at IdService level

--- src/id_service/id_service.py
+++ src/id_service/id_service.py
@@ -1,24 +1,39 @@
 from typing import Protocol
-from hashlib import md5
+from copy import deepcopy
+from hashlib import md5, sha256
 import logging
+
+
+
 
 
 class IdService[Protocol]:
     logger = logging.getLogger()
+    exclude_fields: dict[str, list[str]]
 
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
 
 
-    def generate_id(self) -> str:
+    def generate_id(self, data: dict, **kwargs) -> str:
         pass
 
 
-class IdServiceV0:
-    def generate_id(self) -> str:
+    @staticmethod
+    def exclude_fields(data: dict, exclude_fields: list[str]) -> dict:
+        """
+        Given a generic dict 'data' and exclude_fields, a list of JSON paths like
+            ["field1", "field2.subfield", ...]
+        
+        returns a deepcopied version of data with the specified paths excluded.
+        """
+        pass
+
+class IdServiceMd5:
+    def generate_id(self, data: dict, resource_type: str) -> str:
         pass
 
 
-class IdServiceV1:
-    def generate_id(self) -> str:
+class IdServiceSha256:
+    def generate_id(self, data: dict, resource_type: str) -> str:
         pass
